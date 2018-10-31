183770ba93ad3f4a6373d767de4b37a9e1cb5d8e
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-31 01:36:10 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,18 @@
 class Person
+  
+  def name
+    @name
+  end
+  
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(occupation)
+    @job = occupation
+  end
 end
